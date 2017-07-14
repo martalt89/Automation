116482bc876638fde2af46ba27e71e021037b722
mihaimuresan <mihai.muresan@softvision.ro>
2017-07-14 18:53:35 +0300

    adressed CR changes and added new objects to profile and register pages

---
+++ Automation/src/patient/pages/ChooseProfilePage.java
@@ -0,0 +1,51 @@
+package patient.pages;
+
+import com.heal.framework.web.CommonWebElement;
+import com.heal.framework.web.WebBase;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+
+/**
+ * Created by mihai.muresan on 7/13/2017.
+ */
+public class ChooseProfilePage extends WebBase {
+    public static final String URL = "https://patient.qa.heal.com/profiles";
+
+    ///////////////////
+    // Page Elements //
+    ///////////////////
+
+    //TODO: Also add objects for selected/unselected profile(s) icon(s)
+    public CommonWebElement oProfileName = new CommonWebElement( "oProfileName", "xpath=//*[@id='choose-profile']//h[text()='Mihai']", oWebDriver );
+    public CommonWebElement oManageProfilesLabel = new CommonWebElement( "oManageProfilesLabel", "xpath=//*[text()='Manage Profiles']", oWebDriver );
+    public CommonWebElement oChooseProfileLabel = new CommonWebElement( "oChooseProfileLabel", "xpath=//*[text()='Choose Profile']", oWebDriver );
+    public CommonWebElement oMainProfileLink = new CommonWebElement( "oMainProfileLink", "xpath=//*[@id='choose-profile']//span[1]", oWebDriver );
+    public CommonWebElement oAddPatientLogo = new CommonWebElement( "oAddPatientLogo", "className='create-patient-link", oWebDriver );
+    public CommonWebElement oAddPatientLabel = new CommonWebElement( "oAddPatientLabel", "xpath=//*[text()='Add Patient']", oWebDriver );
+    public CommonWebElement oContinueBtn = new CommonWebElement( "oContinueBtn", "xpath=//*[text()='Continue']", oWebDriver );
+
+    //////////////////
+    // Constructors //
+    //////////////////
+
+    @Parameters({ "url" })
+    public ChooseProfilePage(WebDriver oTargetDriver)
+    {
+        super(oTargetDriver, URL);
+    }
+    public ChooseProfilePage(WebDriver oTargetDriver, String sUrl)
+    {
+        super(oTargetDriver, sUrl);
+    }
+
+    /////////////
+    // Methods //
+    /////////////
+
+    public void selectMainProfile()
+    {
+        //Note2: this method can be optimized to select a given profile as argument
+        this.oMainProfileLink.click();
+        this.oContinueBtn.click();
+    }
+}
---
+++ Automation/src/patient/tests/ChooseProfileTest.java
@@ -0,0 +1,36 @@
+package patient.tests;
+
+import com.heal.framework.web.CommonWebElement;
+import com.heal.framework.web.CommonWebValidate;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import patient.pages.*;
+import utilities.DriverManager;
+
+/**
+ * Created by mihai.muresan on 7/13/2017.
+ */
+public class ChooseProfileTest {
+
+    @Test
+    @Parameters({ "url" })
+    public void checkPofilesPageElements(String url) throws Exception {
+        WebDriver dr = DriverManager.getDriver();
+        ChooseProfilePage profile = new ChooseProfilePage(dr);
+        HomePage homePage = new HomePage(dr);
+        Menu menu = new Menu(dr);
+        LoginPage login = new LoginPage(dr);
+        CommonWebValidate validate = new CommonWebValidate(dr);
+        CommonWebElement.setbMonitorMode(true);
+        login.login("mihai.muresan@heal.com","Heal4325");
+        homePage.selectFromMenu(menu.oProfilesLnk);
+        validate.verifyVisible("Verify Manage Profiles label is displayed", profile.oManageProfilesLabel);
+        validate.verifyVisible("Verify Choose Profile label is displayed", profile.oChooseProfileLabel);
+        validate.verifyVisible("Verify Profile name is displayed", profile.oProfileName);
+        validate.verifyVisible("Verify Add Patient logo is displayed", profile.oAddPatientLogo);
+        validate.verifyVisible("Verify Add Patient label is displayed", profile.oAddPatientLabel);
+        validate.verifyVisible("Verify Continue button is displayed", profile.oContinueBtn);
+
+    }
+}
