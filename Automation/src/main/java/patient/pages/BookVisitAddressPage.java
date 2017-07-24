b2a35e77a3cb1c1136899cc48cacabb0347da262
mihaimuresan <mihai.muresan@softvision.ro>
2017-07-24 19:50:10 +0300

    added new methods for add-card and address pages

--- Automation/src/main/java/patient/pages/BookVisitAddressPage.java
+++ Automation/src/main/java/patient/pages/BookVisitAddressPage.java
@@ -4,6 +4,7 @@ import framework.web.CommonWebElement;
 import framework.web.WebBase;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Parameters;
+import utilities.DriverManager;
 
 /**
  * Created by mihai.muresan on 7/19/2017.
@@ -18,6 +19,7 @@ public class BookVisitAddressPage extends WebBase {
     public CommonWebElement oBookVisitTitle = new CommonWebElement( "oBookVisitTitle", "xpath=//*[text()='Book Visit']", oWebDriver );
     public CommonWebElement oVisitLocationTitle = new CommonWebElement( "oVisitLocationTitle", "xpath=//*[text()='Visit Location']", oWebDriver );
     public CommonWebElement oSavedAddressText = new CommonWebElement( "oSavedAddressText", "xpath=//*[text()='My Saved Address']", oWebDriver );
+    public CommonWebElement oSavedAddress = new CommonWebElement( "oSavedAddress", "className=address-selection", oWebDriver );
     public CommonWebElement oSearchAddressText = new CommonWebElement( "oSearchAddressText", "xpath=//*[text()='Search Address']", oWebDriver );
     public CommonWebElement oWhatAdressText = new CommonWebElement( "oWhatAdressText", "xpath=//*[contains(text(),'What kind')]", oWebDriver );
     public CommonWebElement oAddressInput = new CommonWebElement( "oAddressInput", "name=address1", oWebDriver );
@@ -25,6 +27,7 @@ public class BookVisitAddressPage extends WebBase {
     public CommonWebElement oInstructionsInput = new CommonWebElement( "oInstructionsInput", "name=instructions", oWebDriver );
     public CommonWebElement oSelectAddressType = new CommonWebElement( "oAddressTypeMenu", "className=md-select-value", oWebDriver );
     public CommonWebElement oContinueBtn = new CommonWebElement( "oContinueBtn", "xpath=//*[text()='Continue']", oWebDriver );
+
     //////////////////
     // Constructors //
     //////////////////
@@ -37,6 +40,29 @@ public class BookVisitAddressPage extends WebBase {
     public BookVisitAddressPage(WebDriver oTargetDriver, String sUrl)
     {
         super(oTargetDriver, sUrl);
+    }
+
+    /////////////
+    // Methods //
+    /////////////
+
+    /**
+     *
+     * @param SavedAddress (Boolean) - If true, clicks on the saved address and automatically types
+     *                    the Saved Address in the Address field
+     * @param sAddress (String) - if !SavedAddress, the Address to be used
+     * @param sAptSte (String) - Apt/Ste to be used
+     * @param sInstructions (String) - Instructions to be used
+     * @param sAddressType (String) - Address type to be used
+     */
+    public void typeAddressDetailsAndSubmit(Boolean SavedAddress, String sAddress, String sAptSte, String sInstructions, String sAddressType)
+    {
+        if(SavedAddress) this.oSavedAddress.click();
+        else this.oAddressInput.sendKeys(sAddress);
+        this.oAptSteInput.sendKeys(sAptSte);
+        this.oInstructionsInput.sendKeys(sInstructions);
+        //TODO: Add code to select Address type from dropdown menu - currently there are some issues with this
+        this.oContinueBtn.click();
     }
 
 }
