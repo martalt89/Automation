3b407dc89f09de7ffe279bf54dc51bf8069d7a1c
Adrian Rosu <adrian.rosu@softvision.ro>
2017-08-22 18:16:38 +0300

    Finish Visits page objects Added first tests for creating new visit

--- Automation/src/main/java/ops/pages/CreateVisitPage.java
+++ Automation/src/main/java/ops/pages/CreateVisitPage.java
@@ -84,6 +84,109 @@ public class CreateVisitPage extends WebBase {
 
     // Select / Create address card
 
+        // title
+    public CommonWebElement oSelectAddressTitle = new CommonWebElement("oSelectAddressTitle", "xpath=//h2[text()='Select Address']", oWebDriver);
+    public CommonWebElement oCreateAddressTitle = new CommonWebElement("oCreatePatientTitle", "xpath=//*[@id='react-create-visit-component']//text()[. = 'Create Address']", oWebDriver);
+
+        // buttons
+    public CommonWebElement oSelectFirstAddressBtn = new CommonWebElement("oSelectFirstAddressBtn", "xpath=//button[@class='address-option btn btn-default btn-block'][1]", oWebDriver);
+    public CommonWebElement oCancelBtn = new CommonWebElement("oCancelBtn", "xpath=//button[text()='Cancel']", oWebDriver);
+    public CommonWebElement oSaveAddressBtn = new CommonWebElement("oSaveAddressBtn", "xpath=//button[text()='Save Address']", oWebDriver);
+
+        //labels
+    public CommonWebElement oSearchAddressLabel = new CommonWebElement("oSearchAddressLabel", "xpath=//label[text()='Search Address']", oWebDriver);
+    public CommonWebElement oAddressTypeLabel = new CommonWebElement("oAddressTypeLabel", "xpath=//label[text()='Address Type']", oWebDriver);
+    public CommonWebElement oAddress1Label = new CommonWebElement("oAddress1Label", "xpath=//label[text()='Address 1']", oWebDriver);
+    public CommonWebElement oAddress2Label = new CommonWebElement("oAddress2Label", "xpath=//label[text()='Address 2']", oWebDriver);
+    public CommonWebElement oCityLabel = new CommonWebElement("oCityLabel", "xpath=//label[text()='City']", oWebDriver);
+    public CommonWebElement oStateLabel = new CommonWebElement("oStateLabel", "xpath=//label[text()='State']", oWebDriver);
+    public CommonWebElement oCountryLabel = new CommonWebElement("oCountryLabel", "xpath=//label[text()='Country']", oWebDriver);
+    public CommonWebElement oEstablishmentLabel = new CommonWebElement("oEstablishmentLabel", "xpath=//label[text()='Establishment']", oWebDriver);
+    public CommonWebElement oInstructionsLabel = new CommonWebElement("oInstructionsLabel", "xpath=//label[text()='Instructions']", oWebDriver);
+
+        // fields
+    public CommonWebElement oSearchAddressField = new CommonWebElement("oSearchAddressField", "xpath=//*[contains(@placeholder,'Search Address')]", oWebDriver);
+    public CommonWebElement oAddress1Field = new CommonWebElement("oAddress1Field", "xpath=//*[contains(@placeholder,'Enter Address 1')]", oWebDriver);
+    public CommonWebElement oAddress2Field = new CommonWebElement("oAddress2Field", "xpath=//*[contains(@placeholder,'Enter Address (Unit, Apt#, etc.)')]", oWebDriver);
+    public CommonWebElement oCityField = new CommonWebElement("oCityField", "xpath=//*[contains(@placeholder,'Enter City')]", oWebDriver);
+    public CommonWebElement oStateField = new CommonWebElement("oStateField", "xpath=//*[contains(@placeholder,'Enter State')]", oWebDriver);
+    public CommonWebElement oCountryField = new CommonWebElement("oCountryField", "xpath=//*[contains(@placeholder,'Enter Country')]", oWebDriver);
+    public CommonWebElement oEstablishmentField = new CommonWebElement("oEstablishmentField", "xpath=//*[contains(@placeholder,'Enter Establishment')]", oWebDriver);
+    public CommonWebElement oInstructionsField = new CommonWebElement("oInstructionsField", "xpath=//*[contains(@placeholder,'Enter Instructions')]", oWebDriver);
+
+        // radio buttons
+    public CommonWebElement oHomeOption = new CommonWebElement("oHomeOption", "xpath=//*[@class='radio-group']/input[1]", oWebDriver);
+    public CommonWebElement oWorkOption = new CommonWebElement("oWorkOption", "xpath=//*[@class='radio-group']/input[2]", oWebDriver);
+    public CommonWebElement oOtherOption = new CommonWebElement("oOtherOption", "xpath=//*[@class='radio-group']/input[3]", oWebDriver);
+
+    // Select / Create payment card
+
+        // title
+    public CommonWebElement oSelectPaymentTitle = new CommonWebElement("oSelectPaymentTitle", "xpath=//h2[text()='Select Payment']", oWebDriver);
+    public CommonWebElement oCreatePaymentTitle = new CommonWebElement("oCreatePatientTitle", "xpath=//*[@id='react-create-visit-component']//text()[. = 'Create Payment']", oWebDriver);
+
+        // buttons
+    public CommonWebElement oNewPaymentBtn = new CommonWebElement("oNewPaymentBtn", "xpath=//button[text()='New Payment']", oWebDriver);
+    public CommonWebElement oCreatePaymentBtn = new CommonWebElement("oCreatePaymentBtn", "xpath=//button[text()='Create Payment']", oWebDriver);
+
+        // radio buttons
+    public CommonWebElement oFirstCardOption = new CommonWebElement("oFirstCardOption", "xpath=//*[@class='payment-selection'][1]//input", oWebDriver);
+
+        // fields
+    public CommonWebElement oCardNumberField = new CommonWebElement("oCardNumberField", "xpath=//*[contains(@placeholder,'Enter Card Number')]", oWebDriver);
+    public CommonWebElement oExpDateField = new CommonWebElement("oExpDateField", "xpath=//*[contains(@placeholder,'Enter Expiration Date')]", oWebDriver);
+    public CommonWebElement oCVCField = new CommonWebElement("oCVCField", "xpath=//*[contains(@placeholder,'Enter CVC')]", oWebDriver);
+
+        //labels
+    public CommonWebElement oCardNumberLabel = new CommonWebElement("oCardNumberLabel", "xpath=//label[text()='Card Number']", oWebDriver);
+    public CommonWebElement oExpDateLabel = new CommonWebElement("oExpDateLabel", "xpath=//label[text()='Expiration Date']", oWebDriver);
+    public CommonWebElement oCVCLabel = new CommonWebElement("oCVCLabel", "xpath=//label[text()='Enter CVC']", oWebDriver);
+
+    // Select Visit Service card
+
+        // title
+    public CommonWebElement oSelectVisitServiceTitle = new CommonWebElement("oSelectVisitServiceTitle", "xpath=//h2[text()='Select Visit Service']", oWebDriver);
+    public CommonWebElement oEnterVisitDetailsTitle = new CommonWebElement("oEnterVisitDetailsTitle", "xpath=//h2[text()='Enter Visit Details']", oWebDriver);
+
+        // service buttons
+    public CommonWebElement oSickAdultService = new CommonWebElement("oSickAdultService", "xpath=//*[contains(@class,'patient-name') and text()='sick adult']", oWebDriver);
+    public CommonWebElement oANPSAdultService = new CommonWebElement("oANPSAdultService", "xpath=//*[contains(@class,'patient-name') and text()='anps adult']", oWebDriver);
+    public CommonWebElement oPCPService = new CommonWebElement("oPCPService", "xpath=//*[contains(@class,'patient-name') and text()='pcp']", oWebDriver);
+
+        // fields
+    public CommonWebElement oSymptomsField = new CommonWebElement("oSymptomsField", "xpath=//*[contains(@placeholder,'Enter Symptoms')]", oWebDriver);
+    public CommonWebElement oTimeSlotDropDown = new CommonWebElement("oRelationShipDropDown", "xpath=//select[@class='form-control']/option']", oWebDriver);
+
+    // Visit Price Summary card
+
+        // title
+    public CommonWebElement oVisitPriceTitle = new CommonWebElement("oVisitPriceTitle", "xpath=//h2[text()='Visit Price Summary']", oWebDriver);
+
+        // labels
+    public CommonWebElement oNoPromoLabel = new CommonWebElement("oNoPromoLabel", "xpath=//div[text()='No Promo Code.']", oWebDriver);
+    public CommonWebElement oVisitPriceLabel = new CommonWebElement("oVisitPriceLabel", "xpath=//div[text()='Visit Price']", oWebDriver);
+    public CommonWebElement oDiscountLabel = new CommonWebElement("oDiscountLabel", "xpath=//div[text()='Discount']", oWebDriver);
+    public CommonWebElement oTotalFeeLabel = new CommonWebElement("oTotalFeeLabel", "xpath=//div[text()='Your visit has a flat fee of']", oWebDriver);
+
+    public CommonWebElement oPrice = new CommonWebElement("oPrice", "xpath=//*[contains(@class,'visit-details')]/div[2]/div[2]", oWebDriver);
+    public CommonWebElement oDiscount = new CommonWebElement("oDiscount", "xpath=//*[contains(@class,'visit-details')]/div[3]/div[2]", oWebDriver);
+    public CommonWebElement oTotal = new CommonWebElement("oTotal", "xpath=//*[contains(@class,'visit-details')]/div[4]/div[2]", oWebDriver);
+
+        // buttons
+    public CommonWebElement oApplyPromoBtn = new CommonWebElement("oApplyPromoBtn", "xpath=//button[text()='Apply Promo Code']", oWebDriver);
+    public CommonWebElement oBookVisitBtn = new CommonWebElement("oBookVisitBtn", "xpath=//button[text()='Book Visit']", oWebDriver);
+
+        // fields
+    public CommonWebElement oPromoCodeField = new CommonWebElement("oPromoCodeField", "xpath=//*[contains(@placeholder,'Promo Code')]", oWebDriver);
+
+    // LEFT MENU BUTTONS
+    public CommonWebElement oSelectUserMenu = new CommonWebElement("oSelectUserMenu", "xpath=//*[contains(@class,'no-data') and text()='Select User']", oWebDriver);
+    public CommonWebElement oSelectPatientMenu = new CommonWebElement("oSelectPatientMenu", "xpath=//*[contains(@class,'no-data') and text()='Select Patient']", oWebDriver);
+    public CommonWebElement oSelectAddressMenu = new CommonWebElement("oSelectAddressMenu", "xpath=//*[contains(@class,'no-data') and text()='Select Address']", oWebDriver);
+    public CommonWebElement oAddVisitDetailsMenu = new CommonWebElement("oAddVisitDetailsMenu", "xpath=//*[contains(@class,'no-data') and text()='Add Visit Details']", oWebDriver);
+    public CommonWebElement oSelectPaymentMenu = new CommonWebElement("oSelectPaymentMenu", "xpath=//*[contains(@class,'no-data') and text()='Select Payment']", oWebDriver);
+    public CommonWebElement oVisitSumaryMenu = new CommonWebElement("oVisitSumaryMenu", "xpath=//*[contains(@class,'no-data') and text()='Visit Summary']", oWebDriver);
+
 
     //////////////////
     // Constructors //
@@ -97,9 +200,28 @@ public class CreateVisitPage extends WebBase {
         super(oTargetDriver, sUrl);
     }
 
+
     /////////////
     // Methods //
     /////////////
-
+    /**
+     * Creates new user when adding a visit.
+     *
+     * @param sFirstName (String) - First name to be used.
+     * @param sLastName (String) - Last name to be used.
+     * @param sEmail (String) - User's email address.
+     * @param sPhone (String) - User's phone number.
+     * @param sZipCode (String) - Zip Code to be used.
+     */
+    public void createUser(String sFirstName, String sLastName, String sPhone, String sEmail, String sZipCode)
+    {
+        this.oFirstNameField.sendKeys(sFirstName);
+        this.oLastNameField.sendKeys(sLastName);
+        this.oPhoneField.sendKeys(sPhone);
+        this.oEmailField.sendKeys(sEmail);
+        this.oZipCodeField.sendKeys(sZipCode);
+        this.oCreateUserBtn.click();
+    }
+    // todo methods for: create patient, create address, add visit details, add payments and visit summary
 
 }
