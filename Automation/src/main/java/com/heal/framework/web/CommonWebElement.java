f719e8b3f7f88442ec8a32b7ab0bc7d60dbdf0a9
Vahan <vahan.melikyan@heal.com>
2017-09-15 23:38:56 -0700

    no message

--- Automation/src/main/java/com/heal/framework/web/CommonWebElement.java
+++ Automation/src/main/java/com/heal/framework/web/CommonWebElement.java
@@ -802,7 +802,6 @@ public class CommonWebElement implements WebElement, Locatable {
      * @param sText (String) - Item text
      */
     public void selectByVisibleTextAngular(String sText) {
-        scrollForElement();
         if (this.getTagName().contains("md-select") || this.getTagName().contains("md-select-value") ) //dropdown buttons have md-select or md-select-value tags
             this.click();
         else
