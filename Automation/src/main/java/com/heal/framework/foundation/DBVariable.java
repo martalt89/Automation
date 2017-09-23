b78fe769b814fb23a55d87bed4ccea35b8d990a8
vmelikyan <vahan.melikyan@gmail.com>
2017-09-23 12:39:06 +0800

    DB check module updates, in progress

--- Automation/src/main/java/com/heal/framework/foundation/DBVariable.java
+++ Automation/src/main/java/com/heal/framework/foundation/DBVariable.java
@@ -11,32 +11,35 @@ public class DBVariable {
     //private static Map<String, List<String>> dbVariable = new HashMap<String, List<String>>();
     private static ArrayList<HashMap<String, String>> dbVariable = new ArrayList<HashMap<String, String>>();
 
-    public String getValue(String columnName){
-        return getValue(0, columnName);
+    public static String getCellValue(String columnName){
+        return getCellValue(0, columnName);
     }
 
-    public String getValue(int row, String columnName){
+    public static String getCellValue(int row, String columnName){
 
         return dbVariable.get(row).get(columnName);
     }
 
-    public HashMap<String, String> getFirstRowResponse(){
+    public static HashMap<String, String> getFirstRowResponse(){
         return dbVariable.get(0);
     }
 
-    public HashMap<String, String> getRowResponse(int row){
+    public static HashMap<String, String> getRowResponse(int row){
         return dbVariable.get(row);
     }
 
-    public static void setValue(String columnName, String value){
-        setValue(0, columnName, value);
+    public static void setCellValue(String columnName, String value){
+        setCellValue(0, columnName, value);
     }
 
-    public static void setValue(int row, String columnName, String value){
-
-        HashMap<String, String> rowResponse = dbVariable.get(row);
+    public static void setCellValue(int row, String columnName, String value){
+        HashMap<String, String> rowResponse;
+        if(dbVariable.size()<= row){
+            rowResponse = new HashMap<String, String>();
+            dbVariable.add(rowResponse);
+        }
+        rowResponse = dbVariable.get(row);
         rowResponse.put(columnName, value);
-
     }
 
 }
